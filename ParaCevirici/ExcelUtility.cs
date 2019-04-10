9b18a215447e5113e4f3de8f67e6f2ae004a84df
Hüseyin Kazım Tosun <huseyinkazim@hotmail.com>
2019-04-10 15:24:16 +0300

    2.deploy

---
+++ ParaCevirici/ExcelUtility.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParaCevirici
+{
+    public class ExcelUtility
+    {
+
+        public static void ExportToExcel<T>(List<T> table)
+        {
+           
+            // Load Excel application
+            Microsoft.Office.Interop.Excel.Application excel = new Microsoft.Office.Interop.Excel.Application();
+
+            // Create empty workbook
+            excel.Workbooks.Add();
+
+            // Create Worksheet from active sheet
+            Microsoft.Office.Interop.Excel._Worksheet workSheet = excel.ActiveSheet;
+            workSheet.Name = DateTime.Now.Day.ToString()+".Day";
+            // I created Application and Worksheet objects before try/catch,
+            // so that i can close them in finnaly block.
+            // It's IMPORTANT to release these COM objects!!
+            try
+            {
+                // ------------------------------------------------
+                // Creation of header cells
+                // ------------------------------------------------
+
+                var type = table.First().GetType();
+                int count = 1;
+                foreach (var prop in type.GetProperties())
+                {
+                    workSheet.Cells[1, count] = prop.Name;
+                    count++;
+                }
+
+
+                // ------------------------------------------------
+                // Populate sheet with some real data from "cars" list
+                // ------------------------------------------------
+                int row = 2; // start row (in row 1 are header cells)
+                foreach (var item in table)
+                {
+                    count = 1;
+                    foreach (var prop in type.GetProperties())
+                    {
+                        workSheet.Cells[row, count] = type.GetProperty(prop.Name).GetValue(item);
+                        count++;
+                    }
+                    row++;
+
+                }
+
+                // Apply some predefined styles for data to look nicely :)
+                workSheet.Range["A1"].AutoFormat(Microsoft.Office.Interop.Excel.XlRangeAutoFormat.xlRangeAutoFormatClassic1);
+
+                var filePath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)+"\\Currencies";
+                // Define filename
+                if (!Directory.Exists(filePath))
+                    Directory.CreateDirectory(filePath);
+                if (!Directory.Exists(filePath+ "\\" + DateTime.Now.Year))
+                    Directory.CreateDirectory(filePath+ "\\" + DateTime.Now.Year);
+                string fileName = string.Format(@"{0}\{1}-Month.xlsx", filePath +"\\"+ DateTime.Now.Year, DateTime.Now.Month);
+
+                // Save this data as a file
+                workSheet.SaveAs(fileName);
+
+                // Display SUCCESS message
+                Console.WriteLine(string.Format("The file '{0}' is saved successfully!", fileName));
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine("Exception",
+                    "There was a PROBLEM saving Excel file!\n" + exception.Message);
+            }
+            finally
+            {
+                // Quit Excel application
+                excel.Quit();
+
+                // Release COM objects (very important!)
+                if (excel != null)
+                    System.Runtime.InteropServices.Marshal.ReleaseComObject(excel);
+
+                if (workSheet != null)
+                    System.Runtime.InteropServices.Marshal.ReleaseComObject(workSheet);
+
+                // Empty variables
+                excel = null;
+                workSheet = null;
+
+                // Force garbage collector cleaning
+                GC.Collect();
+            }
+        }
+    }
+}
--- ParaCevirici/Program.cs
+++ ParaCevirici/Program.cs
@@ -7,12 +7,21 @@ using System.Xml;
 using System.Web.Script.Serialization;
 using System.Net;
 using System.IO;
+using System.Data;
+using NsExcel = Microsoft.Office.Interop.Excel;
+using System.ComponentModel;
 
 namespace ParaCevirici
 {
-   
+    public class Car
+    {
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public int MaximumSpeed { get; set; }
+    }
     public class Program
     {
+        public static List<Currency> Currencies { get; set; }
         public static string GET(string url)
         {
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
@@ -56,28 +65,43 @@ namespace ParaCevirici
             }
             var tarih = myxml.SelectSingleNode("//Tarih_Date").Attributes["Tarih"].Value.Split(new char[] { '.' });
             XmlNodeList xmlNodeList = myxml.GetElementsByTagName("Currency");
+            //Console.Write("_________________________________________________________________");
+            //Console.Write("|    Unit        |");
+            //Console.Write("     Isim        |");
+            //Console.Write("  CurrencyName    |");
+            //Console.Write("   ForexBuying    |");
+            //Console.Write("   ForexSelling    |");
+            //Console.Write(" BanknoteBuying   |");
+            //Console.Write("BanknoteSellingling|");
+            //Console.Write("   CrossRateUSD   |");
+            //Console.Write(" CrossRateOther   |");
+            //Console.Write("_________________________________________________________________");
+            Currencies = new List<Currency>();
             foreach (XmlNode node in xmlNodeList)
             {
                 var item = TranslationXML.XMLToObject<Currency>(node.OuterXml);
                 var x = item.Unit;
+
+                Currencies.Add(item);
+
                 //yazdir(item);
                 //HEPSİ STRİNG ŞEKLİNDE TANIMLI ÇÜNKÜ SAYI DEĞERLERİNİN NULLABLE OLABİLİRLİĞİ İÇİN ÖNLEM ALINMAMIŞ
-                Console.Write($"Unit:{item.Unit}");//< Unit > 1 </ Unit >
-                Console.Write($"Isim:{item.Isim}");//< Isim > ABD DOLARI </ Isim >
-                Console.Write($"CurrencyName:{item.CurrencyName}");//< CurrencyName > US DOLLAR </ CurrencyName >
-                if (item.ForexBuying != string.Empty)
-                    Console.Write($"ForexBuying:{float.Parse(item.ForexBuying)}");//< ForexBuying > 4.7908 </ ForexBuying >
-                if (item.ForexSelling != string.Empty)
-                    Console.Write($"ForexSelling:{float.Parse(item.ForexSelling)}");//< ForexSelling > 4.7994 </ ForexSelling >
-                if (item.BanknoteBuying != string.Empty)
-                    Console.Write($"BanknoteBuying:{float.Parse(item.BanknoteBuying)}"); //< BanknoteBuying > 4.7875 </ BanknoteBuying >
-                if (item.BanknoteSelling != string.Empty)
-                    Console.Write($"BanknoteSelling:{float.Parse(item.BanknoteSelling)}");//< BanknoteSelling > 4.8066 </ BanknoteSelling >
-                if (item.CrossRateUSD != string.Empty)
-                    Console.Write($"CrossRateUSD:{float.Parse(item.CrossRateUSD)}");//< CrossRateUSD />< CrossRateOther />
-                if (item.CrossRateOther != string.Empty)
-                    Console.Write($"CrossRateOther:{float.Parse(item.CrossRateOther)}");//< CrossRateUSD />< CrossRateOther />
-                Console.WriteLine();
+                //Console.Write($"    {item.Unit}        ");//< Unit > 1 </ Unit >
+                //Console.Write($"     {item.Isim}        ");//< Isim > ABD DOLARI </ Isim >
+                //Console.Write($"{item.CurrencyName}");//< CurrencyName > US DOLLAR </ CurrencyName >
+                //if (item.ForexBuying != string.Empty)
+                //    Console.Write($"        {string.Format("{0:0.00}", float.Parse(item.ForexBuying))}");//< ForexBuying > 4.7908 </ ForexBuying >
+                //if (item.ForexSelling != string.Empty)
+                //    Console.Write($"        {string.Format("{0:0.00}", float.Parse(item.ForexSelling))}");//< ForexSelling > 4.7994 </ ForexSelling >
+                //if (item.BanknoteBuying != string.Empty)
+                //    Console.Write($"        {string.Format("{0:0.00}", float.Parse(item.BanknoteBuying))}"); //< BanknoteBuying > 4.7875 </ BanknoteBuying >
+                //if (item.BanknoteSelling != string.Empty)
+                //    Console.Write($"        {string.Format("{0:0.00}", float.Parse(item.BanknoteSelling))}");//< BanknoteSelling > 4.8066 </ BanknoteSelling >
+                //if (item.CrossRateUSD != string.Empty)
+                //    Console.Write($"        {string.Format("{0:0.00}", float.Parse(item.CrossRateUSD))}");//< CrossRateUSD />< CrossRateOther />
+                //if (item.CrossRateOther != string.Empty)
+                //    Console.Write($"        {string.Format("{0:0.00}", float.Parse(item.CrossRateOther))}");//< CrossRateUSD />< CrossRateOther />
+                //Console.WriteLine();
             }
         }
         static void JSONConverter()
@@ -93,14 +117,100 @@ namespace ParaCevirici
                 Console.WriteLine("id: {0}, name: {1}", item.id, item.name);
             }
 
-            var jsonData=GET("http://services.groupkt.com/country/get/all");
+            var jsonData = GET("http://services.groupkt.com/country/get/all");
             RootJson countries = new JavaScriptSerializer().Deserialize<RootJson>(jsonData);
         }
+
+      
         static void Main(string[] args)
         {
-           // XmlConverter();
+            
+            //ExportToExcel();
+            XmlConverter();
+            ExcelUtility.ExportToExcel(Currencies);
+
             JSONConverter();
             Console.ReadKey();
+        }
+
+        public static void ExportToExcel()
+        {
+            List<Car> cars = new List<Car>()
+            {
+                new Car {Name = "Toyota", Color = "Red", MaximumSpeed = 195},
+                new Car {Name = "Honda", Color = "Blue", MaximumSpeed = 224},
+                new Car {Name = "Mazda", Color = "Green", MaximumSpeed = 205}
+            };
+            // Load Excel application
+            Microsoft.Office.Interop.Excel.Application excel = new Microsoft.Office.Interop.Excel.Application();
+
+            // Create empty workbook
+            excel.Workbooks.Add();
+
+            // Create Worksheet from active sheet
+            Microsoft.Office.Interop.Excel._Worksheet workSheet = excel.ActiveSheet;
+
+            // I created Application and Worksheet objects before try/catch,
+            // so that i can close them in finnaly block.
+            // It's IMPORTANT to release these COM objects!!
+            try
+            {
+                // ------------------------------------------------
+                // Creation of header cells
+                // ------------------------------------------------
+                workSheet.Cells[1, "A"] = "Name";
+                workSheet.Cells[1, "B"] = "Color";
+                workSheet.Cells[1, "C"] = "Maximum speed";
+
+                // ------------------------------------------------
+                // Populate sheet with some real data from "cars" list
+                // ------------------------------------------------
+                int row = 2; // start row (in row 1 are header cells)
+                foreach (Car car in cars)
+                {
+                    workSheet.Cells[row, "A"] = car.Name;
+                    workSheet.Cells[row, "B"] = car.Color;
+                    workSheet.Cells[row, "C"] = string.Format("{0} km/h", car.MaximumSpeed);
+
+                    row++;
+                }
+
+                // Apply some predefined styles for data to look nicely :)
+                workSheet.Range["A1"].AutoFormat(Microsoft.Office.Interop.Excel.XlRangeAutoFormat.xlRangeAutoFormatClassic1);
+
+                // Define filename
+                string fileName = string.Format(@"{0}\Currency{1}.xlsx", Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), DateTime.Now.Month);
+
+                // Save this data as a file
+                workSheet.SaveAs(fileName);
+
+                // Display SUCCESS message
+                Console.WriteLine(string.Format("The file '{0}' is saved successfully!", fileName));
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine("Exception",
+                    "There was a PROBLEM saving Excel file!\n" + exception.Message);
+            }
+            finally
+            {
+                // Quit Excel application
+                excel.Quit();
+
+                // Release COM objects (very important!)
+                if (excel != null)
+                    System.Runtime.InteropServices.Marshal.ReleaseComObject(excel);
+
+                if (workSheet != null)
+                    System.Runtime.InteropServices.Marshal.ReleaseComObject(workSheet);
+
+                // Empty variables
+                excel = null;
+                workSheet = null;
+
+                // Force garbage collector cleaning
+                GC.Collect();
+            }
         }
     }
 }
